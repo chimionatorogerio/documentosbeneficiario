f2c437a0792ca117e1a10b67f485715094a554fb
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-12-02 18:19:26 -0300

    BeneficiarioRepository: criei método

--- src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/repository/BeneficiarioRepository.java
+++ src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/repository/BeneficiarioRepository.java
@@ -2,6 +2,9 @@ package com.chimionato.beneficiariodocumento.beneficiario.application.repository
 
 import com.chimionato.beneficiariodocumento.beneficiario.domain.Beneficiario;
 
+import java.util.List;
+
 public interface BeneficiarioRepository {
     Beneficiario postBeneficiario(Beneficiario beneficiario);
+    List<Beneficiario> getTodosBeneficiarios();
 }
