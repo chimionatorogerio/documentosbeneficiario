47319a72ff63d8fa60396ed8c51b2f16dce106fc
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-12-02 10:36:03 -0300

    inicia projeto

---
+++ src/main/java/com/chimionato/beneficiariodocumento/BeneficiariodocumentoApplication.java
@@ -0,0 +1,13 @@
+package com.chimionato.beneficiariodocumento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiariodocumentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiariodocumentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chimionato/beneficiariodocumento/BeneficiariodocumentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimionato.beneficiariodocumento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficiariodocumentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
