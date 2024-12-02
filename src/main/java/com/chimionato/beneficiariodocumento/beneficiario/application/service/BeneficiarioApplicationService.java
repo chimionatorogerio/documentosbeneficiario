5397b9313e80844e834d75cdd859c772e9479b82
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-12-02 17:06:33 -0300

    BeneficiarioApplicationService: teste logs Postman ok - criei método e retorno

--- src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioApplicationService.java
+++ src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioApplicationService.java
@@ -1,5 +1,6 @@
 package com.chimionato.beneficiariodocumento.beneficiario.application.service;
 
+import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioListResponse;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioRequest;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioResponse;
 import com.chimionato.beneficiariodocumento.beneficiario.application.repository.BeneficiarioRepository;
@@ -7,6 +8,8 @@ import com.chimionato.beneficiariodocumento.beneficiario.domain.Beneficiario;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Log4j2
@@ -23,5 +26,13 @@ public class BeneficiarioApplicationService implements BeneficiarioService {
         return BeneficiarioResponse.builder()
                 .idBeneficiario(beneficiario.getIdBeneficiario())
                 .build();
+    }
+
+    @Override
+    public List<BeneficiarioListResponse> getTodosBeneficiarios() {
+        log.info("[inicia]     BeneficiarioApplicationService - getTodosBeneficiarios");
+        List<Beneficiario> beneficiarios = beneficiarioRepository.getTodosBeneficiarios();
+        log.info("[FINALIZA]   BeneficiarioApplicationService - getTodosBeneficiarios");
+        return BeneficiarioListResponse.converte(beneficiarios);
     }
 }
