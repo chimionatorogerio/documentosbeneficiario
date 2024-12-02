0b36e294d6d5c65f31a1e85a58caf4b6f257b6f1
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-12-02 18:18:18 -0300

    BeneficiarioListResponse: criei construtor e método converte

--- src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/api/BeneficiarioListResponse.java
+++ src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/api/BeneficiarioListResponse.java
@@ -1,11 +1,30 @@
 package com.chimionato.beneficiariodocumento.beneficiario.application.api;
 
-import java.time.LocalDate;
-import java.util.UUID;
+import com.chimionato.beneficiariodocumento.beneficiario.domain.Beneficiario;
+import lombok.Value;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Value
 public class BeneficiarioListResponse {
     private UUID idBeneficiario;
     private String nomeCompleto;
     private String telefone;
     private LocalDate dataNascimento;
+
+    public static List<BeneficiarioListResponse> converte(List<Beneficiario> beneficiarios) {
+        return beneficiarios.stream()
+                .map(BeneficiarioListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    private BeneficiarioListResponse(Beneficiario beneficiario) {
+        this.idBeneficiario = beneficiario.getIdBeneficiario();
+        this.nomeCompleto = beneficiario.getNomeCompleto();
+        this.telefone = beneficiario.getTelefone();
+        this.dataNascimento = beneficiario.getDataNascimento();
+    }
 }
