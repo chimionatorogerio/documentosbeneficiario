20f303760b4e997907afa5e8d3fc5b974caeb131
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-12-03 15:23:15 -0300

    BeneficiarioService: criei método

--- src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioService.java
+++ src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioService.java
@@ -3,10 +3,13 @@ package com.chimionato.beneficiariodocumento.beneficiario.application.service;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioListResponse;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioRequest;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioResponse;
+import com.chimionato.beneficiariodocumento.documento.domain.Documento;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface BeneficiarioService {
     BeneficiarioResponse postBeneficiario(BeneficiarioRequest beneficiarioRequest);
     List<BeneficiarioListResponse> getTodosBeneficiarios();
+    List<Documento> getDocsUmBeneficiarioPeloId(UUID idBeneficiario);
 }
