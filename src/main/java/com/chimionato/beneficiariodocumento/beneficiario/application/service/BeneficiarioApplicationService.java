8fd602cdc179136f622896bcddb5572edfa44268
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-12-03 15:34:46 -0300

    BeneficiarioApplicationService: teste logs Postman ok - incluí método e retorno

--- src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioApplicationService.java
+++ src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioApplicationService.java
@@ -5,11 +5,13 @@ import com.chimionato.beneficiariodocumento.beneficiario.application.api.Benefic
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioResponse;
 import com.chimionato.beneficiariodocumento.beneficiario.application.repository.BeneficiarioRepository;
 import com.chimionato.beneficiariodocumento.beneficiario.domain.Beneficiario;
+import com.chimionato.beneficiariodocumento.documento.domain.Documento;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Log4j2
@@ -34,5 +36,13 @@ public class BeneficiarioApplicationService implements BeneficiarioService {
         List<Beneficiario> beneficiarios = beneficiarioRepository.getTodosBeneficiarios();
         log.info("[FINALIZA]   BeneficiarioApplicationService - getTodosBeneficiarios");
         return BeneficiarioListResponse.converte(beneficiarios);
+    }
+
+    @Override
+    public List<Documento> getDocsUmBeneficiarioPeloId(UUID idBeneficiario) {
+        log.info("[inicia]     BeneficiarioApplicationService - getDocsUmBeneficiarioPeloId");
+        List<Documento> documentos = beneficiarioRepository.getDocsUmBeneficiarioPeloId(idBeneficiario);
+        log.info("[FINALIZA]   BeneficiarioApplicationService - getDocsUmBeneficiarioPeloId");
+        return documentos;
     }
 }
