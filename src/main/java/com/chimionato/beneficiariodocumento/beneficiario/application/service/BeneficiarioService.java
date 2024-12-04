04b792627a4def34c9a29e29378c0e2b6b462c3e
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-12-04 15:37:03 -0300

    BeneficiarioService: criei método

--- src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioService.java
+++ src/main/java/com/chimionato/beneficiariodocumento/beneficiario/application/service/BeneficiarioService.java
@@ -1,10 +1,12 @@
 package com.chimionato.beneficiariodocumento.beneficiario.application.service;
 
+import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioAlteracaoRequest;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioListResponse;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioRequest;
 import com.chimionato.beneficiariodocumento.beneficiario.application.api.BeneficiarioResponse;
 import com.chimionato.beneficiariodocumento.documento.application.api.DocumentoListResponse;
 import com.chimionato.beneficiariodocumento.documento.domain.Documento;
+import jakarta.validation.Valid;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,4 +15,5 @@ public interface BeneficiarioService {
     BeneficiarioResponse postBeneficiario(BeneficiarioRequest beneficiarioRequest);
     List<BeneficiarioListResponse> getTodosBeneficiarios();
     List<DocumentoListResponse> getDocsUmBeneficiarioPeloId(UUID idBeneficiario);
+    void patchAlteraBeneficiario(UUID idBeneficiario, @Valid BeneficiarioAlteracaoRequest beneficiarioAlteracaoRequest);
 }
